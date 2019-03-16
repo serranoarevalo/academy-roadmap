0113d0daed2fb07d8ad3528c9f3d44f85b1e61f6
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-03-16 10:53:13 +0900

    Updates

--- precache-manifest.22a3d53b54ac39af10165ea661046bdd.js
+++ precache-manifest.22a3d53b54ac39af10165ea661046bdd.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = [
   {
-    "revision": "27d067620b56ac2b1c86",
-    "url": "/academy-roadmap/static/js/main.2979c84b.chunk.js"
+    "revision": "60c0a91b5aa57f59d08a",
+    "url": "/academy-roadmap/static/js/main.98f6019b.chunk.js"
   },
   {
     "revision": "72a8c2d61451575eee14",
     "url": "/academy-roadmap/static/js/runtime~main.2d67f0ff.js"
   },
   {
-    "revision": "189296893522197e8c5a",
-    "url": "/academy-roadmap/static/js/2.bb5e43ab.chunk.js"
+    "revision": "5a7149122891c80dedab",
+    "url": "/academy-roadmap/static/js/2.8941e3c8.chunk.js"
   },
   {
     "revision": "d57d87b5c98bfcb66893b5d1d75e8f58",
     "url": "/academy-roadmap/static/media/Logo.d57d87b5.svg"
   },
   {
-    "revision": "d411cafd6ce6cb9445a19f3ac9c526e1",
+    "revision": "5cf59772f616783703ae3b46f08ced84",
     "url": "/academy-roadmap/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/academy-roadmap/precache-manifest.4be4989e50dcc02b165a731f1cfd7667.js"
+  "/academy-roadmap/precache-manifest.22a3d53b54ac39af10165ea661046bdd.js"
 );
 
 workbox.clientsClaim();
