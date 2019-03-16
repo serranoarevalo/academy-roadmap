98101084182dc5eef83fb92d80febfbc750b16c0
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-03-16 11:46:58 +0900

    Updates

--- precache-manifest.4e1eff451fd14d64a1c8511dcd0c3c2e.js
+++ precache-manifest.4e1eff451fd14d64a1c8511dcd0c3c2e.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = [
   {
-    "revision": "ce4c0ca458ccec772292",
-    "url": "/academy-roadmap/static/js/main.bf4d41bf.chunk.js"
+    "revision": "4bec9de71f0e0cff2a08",
+    "url": "/academy-roadmap/static/js/main.4a93b248.chunk.js"
   },
   {
     "revision": "72a8c2d61451575eee14",
     "url": "/academy-roadmap/static/js/runtime~main.2d67f0ff.js"
   },
   {
-    "revision": "2c7692718dd683d2c04b",
-    "url": "/academy-roadmap/static/js/2.75586d61.chunk.js"
+    "revision": "5a7149122891c80dedab",
+    "url": "/academy-roadmap/static/js/2.8941e3c8.chunk.js"
   },
   {
     "revision": "d57d87b5c98bfcb66893b5d1d75e8f58",
     "url": "/academy-roadmap/static/media/Logo.d57d87b5.svg"
   },
   {
-    "revision": "e30e3d16a5599f00cbdcc8793d37ca59",
+    "revision": "d1aa14dccfb60d17567b2b8bb7c7de43",
     "url": "/academy-roadmap/index.html"
   }
 ];
