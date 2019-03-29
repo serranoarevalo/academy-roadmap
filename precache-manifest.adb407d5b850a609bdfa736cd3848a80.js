a6683b698242f5207c7ef81eb003e62d0b54bdcb
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-03-29 15:17:50 +0900

    Updates

---
+++ precache-manifest.adb407d5b850a609bdfa736cd3848a80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "740836c8b1a9ebf435c5",
+    "url": "/p/roadmap/static/js/main.fa573516.chunk.js"
+  },
+  {
+    "revision": "64bd9326b5c6106ef242",
+    "url": "/p/roadmap/static/js/runtime~main.99b4384c.js"
+  },
+  {
+    "revision": "275f45ad9c14db978ff8",
+    "url": "/p/roadmap/static/js/2.4aa52f3c.chunk.js"
+  },
+  {
+    "revision": "d57d87b5c98bfcb66893b5d1d75e8f58",
+    "url": "/p/roadmap/static/media/Logo.d57d87b5.svg"
+  },
+  {
+    "revision": "911ae4e75123db48efb5b514ff3a2e31",
+    "url": "/p/roadmap/index.html"
+  }
+];
